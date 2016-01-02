1c9c0c41b5f7c2cedad80795dd4968f240f80be0
Angel Rivera <ariv3ra@gmail.com>
2016-01-01 23:05:27 -0500

    initial push with exercises 1-5

---
+++ Makefile
@@ -0,0 +1,11 @@
+
+CFLAGS = -Wall -g
+
+clean:
+	rm -f ex1 ex3 ex4 ex5 
+
+all:
+	make ex1
+	make ex3
+	make ex4
+	make ex5
---
+++ ex1.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main(int argc, char *argv[])
+{
+	puts("Hello world.");
+	puts("My name is Dave.");
+	puts("I'm learing C the hard way.");
+	puts("This is basic stuff but learning C is great.");
+	puts("Looking forward to becoming more profficient");
+
+	return 0;
+}
